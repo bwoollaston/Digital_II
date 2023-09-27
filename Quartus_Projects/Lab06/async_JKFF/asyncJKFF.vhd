9c02ea13ca573e2cb0a1094fb7772ec7385f4975
bwoollaston <woollastoncb@appstate.edu>
2023-09-27 15:43:45 -0400

    lab6 still not goot

--- Quartus_Projects/Lab06/async_JKFF/asyncJKFF.vhd
+++ Quartus_Projects/Lab06/async_JKFF/asyncJKFF.vhd
@@ -9,34 +9,24 @@ ENTITY asyncJKFF IS
 END asyncJKFF;
 
 ARCHITECTURE arch OF asyncJKFF IS
-  SIGNAL Qinternal  : std_logic_vector(1 DOWNTO 0); --internal sig vector <Q,Qbar>
-  SIGNAL s_JK,s_async: std_logic_vector(1 DOWNTO 0); --current value of <J,K> and async inputs <pre,clr>
-  BEGIN
-  s_JK <= i_J & i_K;
-  s_async <= i_LPR & i_LCLR;
-  o_Q <= Qinternal(1);
-  o_Qbar <= Qinternal(0);
-  PROCESS(i_LCLK)
+	SIGNAL Qinternal,s_JK : std_logic_vector(1 DOWNTO 0);
 	BEGIN
-    IF(i_LCLK'EVENT AND i_LCLK='0') THEN
-      CASE s_JK IS
-        WHEN "00" => NULL;
-        WHEN "01" => Qinternal <= "01";
-        WHEN "10" => Qinternal <= "10";
-        WHEN "11" => Qinternal <= not Qinternal;
-        WHEN OTHERS => Qinternal <= (OTHERS =>'1');
-      END CASE;
-    END IF;
-  END PROCESS;
-  PROCESS(i_LCLR,i_LPR)
-  BEGIN
-    CASE s_async IS
-      WHEN "00" => Qinternal <= "11";
-      WHEN "01" => Qinternal <= "10";
-      WHEN "10" => Qinternal <= "01";
-      WHEN "11" => NULL;
-      WHEN OTHERS => Qinternal <= (OTHERS =>'1');
-    END CASE;
-  END PROCESS;
-
+	s_JK <= i_J & i_K;
+	PROCESS(i_LCLK,i_LPR,i_LCLR)
+		BEGIN
+		IF(i_LPR AND i_LCLR = '0') THEN
+			Qinternal(1) <= not i_LPR;
+			Qinternal(0) <= not i_LCLR;
+		ELSIF(i_LCLK'EVENT AND i_LCLK='0') THEN
+			CASE s_JK IS
+				WHEN "00"=>null;
+				WHEN "01"=>Qinternal<="01";
+				WHEN "10"=>Qinternal<="10";
+				WHEN "11"=>Qinternal<=not Qinternal;
+				WHEN OTHERS=> Qinternal<=(OTHERS=>'1');
+			END CASE;
+		END IF;
+	END PROCESS;
+	o_Q <= Qinternal(1);
+	o_Qbar <= Qinternal(0);
 END arch;
