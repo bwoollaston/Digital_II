dbe4d4b475fca55d5f4f8a3fde4470976fe14469
Woollaston <woollastoncb@appstate.edu>
2023-10-30 12:17:04 -0400

    finished mod 10 counter, not turned in

--- Quartus_Projects/Lab09/MOD10_counter/MOD10_Count.vhd
+++ Quartus_Projects/Lab09/MOD10_counter/MOD10_Count.vhd
@@ -17,20 +17,21 @@ ENTITY MOD10_Count IS
 END MOD10_Count;
 
 ARCHITECTURE arch OF MOD10_Count IS
-	SIGNAL s_CLK_DIV	:	std_logic := '0';
+	SIGNAL s_CLK_1Hz	:	std_logic := '0';
 	SIGNAL r_reg		:	unsigned(N-1 DOWNTO 0);
 	SIGNAL r_next		:	unsigned(N-1 DOWNTO 0);
 	
-	freq_div_inst : ENTITY work.freq_div(arch)
-	GENERIC MAP(50000000)
-	PORT MAP(i_CLK_IN=>i_CLK, o_CLK_OUT=>s_CLK_DIV);
-	
 	BEGIN
-	PROCESS(s_CLK_DIV, i_aRST)
+	
+	--Instantiate 50MHz -> 1Hz Freq Divider
+	freq_div_inst : ENTITY work.freq_div(a)
+	PORT MAP(clock_50Mhz=>i_CLK, clock_1Hz=>s_CLK_1Hz);
+	
+	PROCESS(s_CLK_1Hz, i_aRST)
 	BEGIN
 		IF(i_aRST='1') THEN
 				r_reg <= (OTHERS=>'0');
-		ELSIF(s_CLK_DIV'EVENT AND s_CLK_DIV='0') THEN
+		ELSIF(s_CLK_1Hz'EVENT AND s_CLK_1Hz='0') THEN
 				r_reg <= r_next;
 		END IF;
 	END PROCESS;
@@ -45,9 +46,13 @@ ARCHITECTURE arch OF MOD10_Count IS
 				r_next <= unsigned(i_D);
 			END IF;
 		ELSIF(i_EN='1') THEN
-
+			IF(r_reg < 9) THEN
+				r_next <= (r_reg + 1);
+			ELSE 
+				r_next <= (OTHERS=>'0');
+			END IF;
 		ELSE
-		
+			r_next <= r_reg;
 		END IF;
 	END PROCESS;
 	
--- Quartus_Projects/Lab09/MOD10_counter/freq_div.vhd
+++ Quartus_Projects/Lab09/MOD10_counter/freq_div.vhd
@@ -1,31 +1,139 @@
-LIBRARY ieee;
-USE ieee.std_logic_1164.all;
-USE ieee.numeric_std.all;
+-- J. Sid Clements
+-- from Hamblen text Ch.5 CD
+-- modified for 50 MHz clock for DE2-115 instead of 48 MHz
+LIBRARY IEEE;
+USE  IEEE.STD_LOGIC_1164.all;
+USE  IEEE.STD_LOGIC_ARITH.all;
+USE  IEEE.STD_LOGIC_UNSIGNED.all;
 
-ENTITY Freq_div IS
-	GENERIC(
-		c_MAX : INTEGER := 50000000
-	);
-	PORT(
-		i_CLK_IN		: IN	std_logic;
-		o_CLK_OUT	: OUT std_logic
-	);
-END Freq_div;
+ENTITY freq_div IS
 
-ARCHITECTURE arch OF Freq_div IS
-	SIGNAL r_VAL_reg	: unsigned(63 DOWNTO 0) := (OTHERS=>'0');
-	SIGNAL r_VAL_next : unsigned(63 DOWNTO 0) := (OTHERS=>'0');
+	PORT
+	(
+		clock_50Mhz			   : IN	STD_LOGIC;
+		clock_1MHz				: OUT	STD_LOGIC;
+		clock_100KHz			: OUT	STD_LOGIC;
+		clock_10KHz				: OUT	STD_LOGIC;
+		clock_1KHz				: OUT	STD_LOGIC;
+		clock_100Hz				: OUT	STD_LOGIC;
+		clock_10Hz				: OUT	STD_LOGIC;
+		clock_1Hz				: OUT	STD_LOGIC);
+	
+END freq_div;
+
+ARCHITECTURE a OF freq_div IS
+
+	SIGNAL	count_1Mhz: STD_LOGIC_VECTOR(6 DOWNTO 0); 
+	SIGNAL	count_100Khz, count_10Khz, count_1Khz : STD_LOGIC_VECTOR(2 DOWNTO 0);
+	SIGNAL	count_100hz, count_10hz, count_1hz : STD_LOGIC_VECTOR(2 DOWNTO 0);
+	SIGNAL  clock_1Mhz_int, clock_100Khz_int, clock_10Khz_int, clock_1Khz_int: STD_LOGIC; 
+	SIGNAL	clock_100hz_int, clock_10Hz_int, clock_1Hz_int : STD_LOGIC;
+	SIGNAL  clock_1Mhz_reg, clock_100Khz_reg, clock_10Khz_reg, clock_1Khz_reg: STD_LOGIC; 
+	SIGNAL	clock_100hz_reg, clock_10Hz_reg, clock_1Hz_reg : STD_LOGIC;
+BEGIN
+	PROCESS 
 	BEGIN
-	
-	PROCESS(i_CLK_IN)
-		BEGIN
-		IF(i_CLK_IN'EVENT AND i_CLK_IN='0') THEN
-			r_VAL_reg <= r_VAL_next;
-		END IF;
-	END PROCESS;
-	
-	-- NEXT STATE LOGIC
-	r_VAL_next	<= (OTHERS=>'0') 	WHEN r_VAL_reg = to_unsigned(c_MAX,64) ELSE (r_VAL_reg+1);
-	o_CLK_OUT 	<= '1' 				WHEN r_VAL_reg = to_unsigned(c_MAX,64) ELSE '0';			
-	
-END arch;
+			clock_1Mhz <= clock_1Mhz_reg;
+			clock_100Khz <= clock_100Khz_reg;
+			clock_10Khz <= clock_10Khz_reg;
+			clock_1Khz <= clock_1Khz_reg;
+			clock_100hz <= clock_100hz_reg;
+			clock_10hz <= clock_10hz_reg;
+			clock_1hz <= clock_1hz_reg;
+-- Divide by 48
+		WAIT UNTIL clock_50Mhz'EVENT and clock_50Mhz = '1';
+			IF count_1Mhz < 49 THEN
+				count_1Mhz <= count_1Mhz + 1;
+			ELSE
+				count_1Mhz <= "0000000";
+			END IF;
+			IF count_1Mhz < 25 THEN
+				clock_1Mhz_int <= '0';
+			ELSE
+				clock_1Mhz_int <= '1';
+			END IF;	
+
+		-- Ripple clocks are used in this code to save prescalar hardware
+		-- Sync all clock prescalar outputs back to master clock signal
+			clock_1Mhz_reg <= clock_1Mhz_int;
+			clock_100Khz_reg <= clock_100Khz_int;
+			clock_10Khz_reg <= clock_10Khz_int;
+			clock_1Khz_reg <= clock_1Khz_int;
+			clock_100hz_reg <= clock_100hz_int;
+			clock_10hz_reg <= clock_10hz_int;
+			clock_1hz_reg <= clock_1hz_int;
+	END PROCESS;	
+
+		-- Divide by 10
+	PROCESS 
+	BEGIN
+		WAIT UNTIL clock_1Mhz_reg'EVENT and clock_1Mhz_reg = '1';
+			IF count_100Khz /= 4 THEN
+				count_100Khz <= count_100Khz + 1;
+			ELSE
+				count_100khz <= "000";
+				clock_100Khz_int <= NOT clock_100Khz_int;
+			END IF;
+	END PROCESS;	
+
+		-- Divide by 10
+	PROCESS
+	BEGIN
+		WAIT UNTIL clock_100Khz_reg'EVENT and clock_100Khz_reg = '1';
+			IF count_10Khz /= 4 THEN
+				count_10Khz <= count_10Khz + 1;
+			ELSE
+				count_10khz <= "000";
+				clock_10Khz_int <= NOT clock_10Khz_int;
+			END IF;
+	END PROCESS;	
+
+		-- Divide by 10
+	PROCESS 
+	BEGIN
+		WAIT UNTIL clock_10Khz_reg'EVENT and clock_10Khz_reg = '1';
+			IF count_1Khz /= 4 THEN
+				count_1Khz <= count_1Khz + 1;
+			ELSE
+				count_1khz <= "000";
+				clock_1Khz_int <= NOT clock_1Khz_int;
+			END IF;
+	END PROCESS;	
+
+		-- Divide by 10
+	PROCESS 
+	BEGIN
+		WAIT UNTIL clock_1Khz_reg'EVENT and clock_1Khz_reg = '1';
+			IF count_100hz /= 4 THEN
+				count_100hz <= count_100hz + 1;
+			ELSE
+				count_100hz <= "000";
+				clock_100hz_int <= NOT clock_100hz_int;
+			END IF;
+	END PROCESS;	
+
+		-- Divide by 10
+	PROCESS 
+	BEGIN
+		WAIT UNTIL clock_100hz_reg'EVENT and clock_100hz_reg = '1';
+			IF count_10hz /= 4 THEN
+				count_10hz <= count_10hz + 1;
+			ELSE
+				count_10hz <= "000";
+				clock_10hz_int <= NOT clock_10hz_int;
+			END IF;
+	END PROCESS;	
+
+		-- Divide by 10
+	PROCESS
+	BEGIN
+		WAIT UNTIL clock_10hz_reg'EVENT and clock_10hz_reg = '1';
+			IF count_1hz /= 4 THEN
+				count_1hz <= count_1hz + 1;
+			ELSE
+				count_1hz <= "000";
+				clock_1hz_int <= NOT clock_1hz_int;
+			END IF;
+	END PROCESS;	
+
+END a;
